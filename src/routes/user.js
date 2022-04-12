0783b6f662e2200aaed9598323497a4a011e68ad
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-04-12 17:52:19 -0300

    feat: Create User sem validações

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const temporaryServiceUser = require('../services/user');
 
 const router = express.Router();
 
-router.post('/', async (_req, res) => {
-  res.status(200).json({ message: 'Ok' });
+router.post('/', async (req, res) => {
+  const result = await temporaryServiceUser.createUser(req.body);
+  res.status(200).json(result);
 });
 
 module.exports = router;
